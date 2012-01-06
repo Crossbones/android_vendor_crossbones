9b299586c408f8b1642fa91af00f4f2b48ab60b8
Mike Wielgosz <dev.mwielgosz@gmail.com>
2012-01-06 22:30:13 +0000

    maguro: add Crossbones makefile

---
+++ products/crossbones_maguro.mk
@@ -0,0 +1,24 @@
+$(call inherit-product, device/samsung/maguro/full_maguro.mk)
+$(call inherit-product, vendor/crossbones/products/common.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := crossbones_maguro
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := toro
+PRODUCT_MODEL := Galaxy Nexus GSM
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.0.3/IML74K/239410:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.0.3 IML74K 239410 release-keys" BUILD_NUMBER=239410
+
+# Crossbones common version
+-include vendor/crossbones/products/version.mk
+
+PRODUCT_MOTD :="\n\n\n-----------------CROSSBONES---------------------\nCrossbones $(BUILD_VERSION) for your $(PRODUCT_MODEL)\nFollow @Xbones_dev for the latest updates\n------------------------------------------------\n"
+
+# Extra Crespo overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/crossbones/overlay/maguro
+
+# Bootanimation
+PRODUCT_COPY_FILES +=  \
+    vendor/crossbones/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
