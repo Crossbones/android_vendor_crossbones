6af996c0fa5e0640f89d33e0f028e712a77c8ee0
Mike Wielgosz <dev.mwielgosz@gmail.com>
2012-01-06 23:37:45 +0000

    AndroidProducts: add maguro

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -5,5 +5,7 @@ else ifeq ($(TARGET_PRODUCT),crossbones_crespo4g)
   PRODUCT_MAKEFILES := $(LOCAL_DIR)/crossbones_crespo4g.mk
 else ifeq ($(TARGET_PRODUCT),crossbones_toro)
   PRODUCT_MAKEFILES := $(LOCAL_DIR)/crossbones_toro.mk
+else ifeq ($(TARGET_PRODUCT),crossbones_maguro)
+  PRODUCT_MAKEFILES := $(LOCAL_DIR)/crossbones_maguro.mk
 endif
 
