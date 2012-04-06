f711ed4fd16adb6fc89e868d95c0c5ac6cd756f3
Mike Wielgosz <dev.mwielgosz@gmail.com>
2012-04-06 19:17:50 -0400

    generic: add generic makefile for emulator
    Change-Id: Ice7ad6957cd6dd27e9cf6b4993cf2a47ebafd442

---
+++ products/crossbones_generic.mk
@@ -0,0 +1,22 @@
+# Inherit AOSP device configuration for generic target
+$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product, vendor/crossbones/products/common.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := crossbones_generic
+PRODUCT_BRAND := crossbones
+PRODUCT_DEVICE := generic
+PRODUCT_MODEL := CrossbonesROM
+PRODUCT_MANUFACTURER := Crossbones
+
+# Crossbones common version
+-include vendor/crossbones/products/version.mk
+
+PRODUCT_MOTD :="\n\n\n-----------------CROSSBONES---------------------\nCrossbones $(BUILD_VERSION) for your $(PRODUCT_MODEL)\nFollow @Xbones_dev for the latest updates\n------------------------------------------------\n"
+
+#
+# Move dalvik cache to data partition where there is more room to solve startup problems
+#
+PRODUCT_PROPERTY_OVERRIDES += dalvik.vm.dexopt-data-only=1
