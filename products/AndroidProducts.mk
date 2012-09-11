ea28ab4797de677c874433100aa322543a34c1b1
Mike Wielgosz <dev.mwielgosz@gmail.com>
2012-09-11 12:19:19 -0400

    Rework AndroidProducts.mk - makes logical sense
    Change-Id: I939a455ae1ba30dc21cb3a4f6b052569e846b228

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,15 +1,6 @@
-#Temporary work around... * cocide
-ifeq ($(TARGET_PRODUCT),crossbones_generic)
-  PRODUCT_MAKEFILES := $(LOCAL_DIR)/crossbones_generic.mk
-else ifeq ($(TARGET_PRODUCT),crossbones_crespo)
-  PRODUCT_MAKEFILES := $(LOCAL_DIR)/crossbones_crespo.mk
-else ifeq ($(TARGET_PRODUCT),crossbones_crespo4g)
-  PRODUCT_MAKEFILES := $(LOCAL_DIR)/crossbones_crespo4g.mk
-else ifeq ($(TARGET_PRODUCT),crossbones_maguro)
-  PRODUCT_MAKEFILES := $(LOCAL_DIR)/crossbones_maguro.mk
-else ifeq ($(TARGET_PRODUCT),crossbones_toro)
-  PRODUCT_MAKEFILES := $(LOCAL_DIR)/crossbones_toro.mk
-else ifeq ($(TARGET_PRODUCT),crossbones_toroplus)
-  PRODUCT_MAKEFILES := $(LOCAL_DIR)/crossbones_toroplus.mk
-endif
-
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/crossbones_crespo.mk \
+    $(LOCAL_DIR)/crossbones_crespo4g.mk \
+    $(LOCAL_DIR)/crossbones_maguro.mk \
+    $(LOCAL_DIR)/crossbones_toro.mk \
+    $(LOCAL_DIR)/crossbones_toroplus.mk
