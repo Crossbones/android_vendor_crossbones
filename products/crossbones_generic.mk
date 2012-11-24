690cf019b97695cfe0d6f1290cfaf15c56e8af80
Mike Wielgosz <dev.mwielgosz@gmail.com>
2012-11-23 21:09:07 -0500

    generic: change PRODUCT_MOTD to reflect changes to other devices
    Change-Id: Ie6fb011a78bcdeea11608adf7563c6b6eb09375c

--- products/crossbones_generic.mk
+++ products/crossbones_generic.mk
@@ -8,13 +8,13 @@ $(call inherit-product, vendor/crossbones/products/common.mk)
 PRODUCT_NAME := crossbones_generic
 PRODUCT_BRAND := crossbones
 PRODUCT_DEVICE := generic
-PRODUCT_MODEL := CrossbonesROM
+PRODUCT_MODEL := Generic Device
 PRODUCT_MANUFACTURER := Crossbones
 
 # Crossbones common version
 -include vendor/crossbones/products/version.mk
 
-PRODUCT_MOTD :="\n\n\n-----------------CROSSBONES---------------------\nCrossbones $(BUILD_VERSION) for your $(PRODUCT_MODEL)\nFollow @Xbones_dev for the latest updates\n------------------------------------------------\n"
+PRODUCT_MOTD :="\n\n\n-----------------CROSSBONES---------------------\nCrossbones $(BUILD_VERSION) for your Generic Device\nFollow @Xbones_dev for the latest updates\n------------------------------------------------\n"
 
 #
 # Move dalvik cache to data partition where there is more room to solve startup problems
