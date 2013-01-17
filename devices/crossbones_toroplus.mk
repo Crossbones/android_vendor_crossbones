80c7f0064c1e7553873757d1b8bd9dbb3de4b8b8
Mike Wielgosz <dev.mwielgosz@gmail.com>
2013-01-16 23:37:53 -0500

    toroplus: update fingerprint to reflect Sprint OTA also change PRODUCT_BRAND to samsung

--- devices/crossbones_toroplus.mk
+++ devices/crossbones_toroplus.mk
@@ -13,14 +13,11 @@ PRODUCT_COPY_FILES +=  \
     vendor/crossbones/prebuilt/bootanimation/bootanimation-xhdpi.zip:system/media/bootanimation.zip \
     device/samsung/toroplus/vold.fstab:system/etc/vold.fstab
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.camera.res=5MP
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_DISPLAY_ID=JOP40D BUILD_FINGERPRINT="samsung/mysidspr/torospr:4.2.1/JOP40D/533553:user/release-keys" PRIVATE_BUILD_DESC="mysidspr-user 4.2.1 JOP40D 533553 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_DISPLAY_ID=JOP40D BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.2.1/JOP40D/L700GA02:user/release-keys" PRIVATE_BUILD_DESC="mysidspr-user 4.2.1 JOP40D L700GA02 release-keys"
 
 PRODUCT_NAME := crossbones_toroplus
 PRODUCT_DEVICE := toroplus
-PRODUCT_BRAND := google
+PRODUCT_BRAND := samsung
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_RESTRICT_VENDOR_FILES := false
