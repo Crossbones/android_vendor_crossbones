8872bdb8bd2a2fd6b93d6543cadf04da30125860
IT_Blackbelt <jmeade300@gmail.com>
2013-02-04 23:28:29 -0500

    Bump all devices to current build version JOP40G

--- devices/crossbones_toroplus.mk
+++ devices/crossbones_toroplus.mk
@@ -13,7 +13,7 @@ PRODUCT_COPY_FILES +=  \
     vendor/crossbones/prebuilt/bootanimation/bootanimation-xhdpi.zip:system/media/bootanimation.zip \
     device/samsung/toroplus/vold.fstab:system/etc/vold.fstab
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_DISPLAY_ID=JOP40D BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.2.1/JOP40D/L700GA02:user/release-keys" PRIVATE_BUILD_DESC="mysidspr-user 4.2.1 JOP40D L700GA02 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_DISPLAY_ID=JOP40G BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.2.1/JOP40G/L700GA02:user/release-keys" PRIVATE_BUILD_DESC="mysidspr-user 4.2.1 JOP40G L700GA02 release-keys"
 
 PRODUCT_NAME := crossbones_toroplus
 PRODUCT_DEVICE := toroplus
