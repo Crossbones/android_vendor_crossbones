9360450f64524938cf8434bbc0563e41058fcdda
IT_Blackbelt <jmeade300@gmail.com>
2013-05-27 17:40:19 -0400

    Update build number for 4.2.2_r1.2

--- devices/crossbones_toroplus.mk
+++ devices/crossbones_toroplus.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/crossbones/config/common_ledflash.mk)
 PRODUCT_COPY_FILES +=  \
     vendor/crossbones/prebuilt/media/bootanimation/bootanimation-xhdpi.zip:system/media/bootanimation.zip
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_DISPLAY_ID=JDQ39 BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.2.2/JDQ39/L700GA02:user/release-keys" PRIVATE_BUILD_DESC="mysidspr-user 4.2.2 JDQ39 L700GA02 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_DISPLAY_ID=JDQ39E BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.2.2/JDQ39E/L700GA02:user/release-keys" PRIVATE_BUILD_DESC="mysidspr-user 4.2.2 JDQ39E L700GA02 release-keys"
 
 PRODUCT_NAME := crossbones_toroplus
 PRODUCT_DEVICE := toroplus
